527b1420ed4dac806938c0aa1f03ba3c82bfa7ad
rodrigosoberon <rodrigo.soberon.a@gmail.com>
2023-05-22 15:13:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button, Container } from 'react-bootstrap'
+
+function App() {
+	return (
+		<Container className='my-5'>
+			<Button variant='primary'>Bootstrap!</Button>
+		</Container>
+	)
+}
+
+export default App
